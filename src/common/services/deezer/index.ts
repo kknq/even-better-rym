47672ca1bb00866332162d23a6987d68be4aa1ff
Brandon D. <84109908+BrandonDelM@users.noreply.github.com>
2025-12-29 10:02:31 -0800

    Added search to Deezer

---
+++ src/common/services/deezer/index.ts
@@ -0,0 +1,16 @@
+import DeezerIcon from '../../icons/deezer'
+import DeezerFoundIcon from '../../icons/deezer-found'
+import DeezerNotFoundIcon from '../../icons/deezer-notfound'
+import { withCache } from '../../utils/cache'
+import type { Searchable, Service } from '../types'
+import { search } from './search'
+
+export const Deezer: Service & Searchable = {
+    id: 'deezer',
+    name: 'Deezer',
+    regex: /https?:\/\/.*\.deezer\.com\/album\/.*/,
+    icon: DeezerIcon,
+    foundIcon: DeezerFoundIcon,
+    notFoundIcon: DeezerNotFoundIcon,
+    search: withCache('deezer-search', search),
+}
---
+++ src/common/services/deezer/search.ts
@@ -0,0 +1,18 @@
+import { fetch } from '../../utils/fetch'
+import type { SearchFunction } from '../types'
+
+export const search: SearchFunction = async({  artist, title }) => {
+    const response = await fetch({
+        url: `https://tidal.com/search/${artist} ${title}/album`,
+        method: 'GET',
+    })
+
+    const html = new DOMParser().parseFromString(response, 'text/html')
+    const topResult = html.querySelector('[data-testid="album_thumbnail"]')
+    if (!topResult) {
+        return undefined
+    }
+
+    const url = topResult.querySelector('a')?.href
+    return url ?? undefined
+}
