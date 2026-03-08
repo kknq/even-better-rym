997d17b3eb4368055c6a399be1e50e55c3584776
ighaltq1qzq <thekevzner@gmail.com>
2026-03-09 00:01:35 +0100

    Update resolve.ts

--- src/common/services/applemusic/resolve.ts
+++ src/common/services/applemusic/resolve.ts
@@ -14,7 +14,7 @@ import type {
 } from '../types'
 import type { MusicVideoData, ReleaseData } from './codec'
 
-const FULL_IMAGE_SIZE = '999999999x999999999'
+const FULL_IMAGE_SIZE = '9999x9999'
 
 export function convertAppleMusicDuration(appleMusicDuration: string) {
   // Extract minutes and seconds using a regular expression
@@ -131,16 +131,10 @@ export const resolve: ResolveFunction = async (url) => {
 
     coverArt = asArray(
       pipe(
-        document_.querySelector<HTMLSourceElement>(
-          'div[slot="artwork"] source[type="image/jpeg"]',
-        ),
+        document_.querySelector<HTMLMetaElement>('meta[property="og:image"]') ??
+          undefined,
         ifDefined((el) => {
-          // get the last (largest?) srcset entry, take URL before whitespace
-          const srcset = el?.attributes?.srcset?.value
-          if (!srcset) return undefined
-          const lastSrc = srcset.split(',').pop()?.trim()
-          return lastSrc ? lastSrc.split(' ')[0] : undefined
-        }),
+          el.content.replace(/\d+x\d+bf-\d+\.jpg/, `${FULL_IMAGE_SIZE}.jpg`),
       ),
     )
 
