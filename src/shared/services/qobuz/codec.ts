9421227e292bdb402a0c88267e228dc2e7d5926d
kknq <75908194+kknq@users.noreply.github.com>
2026-04-04 20:16:27 +0200

    feat: qobuz submission integration (#26)

---
+++ src/shared/services/qobuz/codec.ts
@@ -0,0 +1,28 @@
+export type ReleaseData = {
+    '@context': string
+    '@type': 'MusicAlbum'
+    '@id': string
+    url: string
+    name: string
+    description: string
+    datePublished: string
+    potentialAction: PotentialAction
+}
+
+export type PotentialAction = {
+    '@type': 'ListenAction'
+    target: Target[]
+    expectsAcceptanceOf: Offer
+}
+
+export type Target = {
+    '@type': 'EntryPoint'
+    urlTemplate: string
+    actionPlatform: string[]
+}
+
+export type Offer = {
+    '@type': 'Offer'
+    category: string
+    elegibleRegion: string[]
+}
--- src/shared/services/qobuz/index.ts
+++ src/shared/services/qobuz/index.ts
@@ -1,16 +1,18 @@
 import { withCache } from '../../utils/cache'
-import type { Searchable, Service } from '../types'
+import type { Resolvable, Searchable, Service } from '../types'
 import QobuzIcon from './icon'
 import QobuzFoundIcon from './icon-found'
 import QobuzNotFoundIcon from './icon-notfound'
+import { resolve } from './resolve'
 import { search } from './search'
 
-export const Qobuz: Service & Searchable = {
+export const Qobuz: Service & Searchable & Resolvable = {
   id: 'qobuz',
   name: 'Qobuz',
-  regex: /https?:\/\/open\.qobuz\.com\/album\//,
+  regex: /https?:\/\/.*\.qobuz\.com\/[a-z]{2}-[a-z]{2}\/album\/.*/,
   icon: QobuzIcon,
   foundIcon: QobuzFoundIcon,
   notFoundIcon: QobuzNotFoundIcon,
   search: withCache('qobuz-search', search),
+  resolve: withCache('qobuz-resolve', resolve),
 }
